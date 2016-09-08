5f695312a61e42f93244a1e5f07f4494c028b6eb
August von Trapp and Matt Wilkin <august+matt@>
2016-09-08 11:43:08 -0700

    add code and spec for update contribution.

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -6,6 +6,26 @@ class ContributionsController < ApplicationController
     @contribution = Contribution.new
   end
 
+  def show
+    @contribution = Contribution.find(params[:id])
+  end
+
+  def edit
+    @story = Story.find(params[:story_id])
+    @contribution = Contribution.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:story_id])
+    @contribution = Contribution.find(params[:id])
+
+    if @contribution.update(contribution_params)
+      redirect_to story_contribution_path()
+    else
+      render :edit
+    end
+  end
+
   def create
     @story = Story.find(params[:story_id])
     @contribution = @story.contributions.new(contribution_params)
--- spec/features/contribution_feature_spec.rb
+++ spec/features/contribution_feature_spec.rb
@@ -10,4 +10,16 @@ describe 'the contribution feature' do
     click_on 'Create Contribution'
     expect(page).to have_content('While reflecting upon the Way, I realized that the true Art of War, is peace.')
   end
+
+  it 'should allow user to edit a contribution' do
+    story = Story.create(title: 'Then I Found Five Dollars')
+    contribution = story.contributions.create(content: 'on the seat of my bicycle', image: 'image1.jpeg', creator: 'Matt')
+    visit story_contribution_path(story, contribution)
+    click_on 'Edit'
+    fill_in 'Creator', with: 'Fred Flintsone'
+    fill_in 'Content', with: 'on the seat of car.'
+    click_on 'Update Contribution'
+    expect(page).to have_content('on the seat of car.')
+  end
+
 end
